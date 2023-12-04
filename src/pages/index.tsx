784a11a5b185ce01f2cb26190604aed304f56b8e
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-12-04 15:46:51 +0100

    fix path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/getting-started/installation">
             Style Starter Kit - 5min ⏱️
           </Link>
         </div>
