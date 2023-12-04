4042b89cc0ce81772942f4d212d00858636e777b
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-12-04 15:58:05 +0100

    add seo image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,8 +55,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/seo-image.jpg',
     navbar: {
       title: 'Style Starter Kit',
       logo: {
